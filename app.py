7d47bd99ad957b32e9de61bf37176518276facc4
d4rk30 <d4rk30@nohacker.me>
2023-12-12 22:46:30 +0800

    更新数据模型，增加创建测试数据方法

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import os
 import sys
 import click
 
-from flask import Flask,render_template,flash,redirect,url_for
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length
+from sqlalchemy import func
+from faker import Faker
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -21,84 +19,196 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 app.secret_key = 'your_secret_key_here'
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
-bootstrap = Bootstrap5(app)
 
-##DB Design
 
-#xxzXZXZXzXzXZXzZXzXZXzX
+# DB设计
+# ---------------------------------------------------------------------------------------
+# 货物数据结构
 class Commodity(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    number = db.Column(db.Interger,unique=False,nullabel=False)
-    type = db.Column(db.String,nullabel=False)
-    price = db.Column(db.Float,nullabel=False)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    # 货号
+    number = db.Column(db.Integer,nullable=False)
+    # 商品名称
+    title = db.Column(db.String(80),nullable=False)
+    # 商品描述
     description = db.Column(db.Text)
-    
-    images = db.relationship('Images',back_populates = 'commodity')
+    # 主图
+    main_image = db.Column(db.Text)
+    # 商品照片
+    images = db.Column(db.JSON)
+    # 型号和价格组，每个货物有不同的型号和售价
+    models = db.relationship('ModelPrice')
 
-class Image(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    path = db.Column(db.Text)
-    
-    commodity_id = db.Column(db.Integer,db.ForeignKey('commodity.id'))
-    commodity = db.relationship('Commodity',back_populates = 'images')
+class ModelPrice(db.Model):
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    # 型号
+    type = db.Column(db.String,nullable=False)
+    # 价格
+    price = db.Column(db.Float,nullable=False)
+    # 外键-所属的货物
+    commodity_id =  db.Column(db.Integer,db.ForeignKey('commodity.id'))
 
+# 用户信息，需要对接微信openID
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
     user_wechat = db.Column(db.Text)
     phone = db.Column(db.Integer)
 
-    orders
-    consignees
+    # 每个用户有自己的订单信息
+    orders =  db.relationship('Order')
+    # 每个用户有自己的地址信息
+    consignees =  db.relationship('Consignee')
 
+# 订单信息
 class Order(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    number = db.Column(db.Interger,unique=False,nullabel=False)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    # 订单号
+    number = db.Column(db.Integer,nullable=False)
+    # 货号
+    commodity_number = db.Column(db.Integer,nullable=False)
+    # 型号
+    type = db.Column(db.String,nullable=False)
+    # 数量
+    conut = db.Column(db.Integer,nullable=False)
+    # 地址
+    address = db.Column(db.Text,nullable=False)
+    # 手机
+    phone = db.Column(db.Integer,nullable=False)
+    # 关联的物流车
+    expressage_id = db.Column(db.Integer,nullable=False)
+    # 收件人
+    name = db.Column(db.String,nullable=False)
+    # 外键-所属的用户
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
+
+# 收获地址信息
 class Consignee(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    address = db.Column(db.Text)
-    phone = db.Column(db.Integer)
-    name = db.Column(db.String,nullabel=False)
-    
-    expressage_id = db.Column(db.Integer,nullabel=False)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    # 地址
+    address = db.Column(db.Text,nullable=False)
+    # 手机
+    phone = db.Column(db.Integer,nullable=False)
+    # 收件人
+    name = db.Column(db.String,nullable=False)
+    # 地址关联的物流车
+    expressage_id = db.Column(db.Integer,nullable=False)
+    # 外键-所属的用户
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
+# 物流车信息
 class Expressage(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    phone = db.Column(db.Integer)
-    license_number = db.Column(db.String,nullabel=False)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    # 物流名字
+    title = db.Column(db.String(80),nullable=False)
+    # 介绍
+    description = db.Column(db.Text)
+    # 联系人
+    name = db.Column(db.String(80),nullable=False)
+    # 手机
+    phone = db.Column(db.Integer,nullable=False)
+    # 车牌号
+    license_number = db.Column(db.String,nullable=False)
 
-@app.route('/')
-def index():
-    form = LForm()
-    return render_template('index.html',form=form)
+# API
+# ---------------------------------------------------------------------------------------
+@app.route('/api/commoditys')
+def get_commoditys():
+    page = request.args.get('page', 1, type=int)         # 页数，默认为第1页
+    per_page = request.args.get('per_page', 10, type=int) # 每页项目数，默认10
 
-@app.route('/')
-def explore():
-    return render_template('index.html')
+    paginated_commoditys = Commodity.query.paginate(page=page, per_page=per_page, error_out=False)
 
-@app.route('/')
-def about():
-    return render_template('index.html')
+    commoditys = []
+    for commodity in paginated_commoditys.items:
+        subquery = ModelPrice.query.with_entities(ModelPrice.price).filter(ModelPrice.commodity_id == commodity.id).subquery()
+        min_price = db.session.query(func.min(subquery.c.price)).scalar()
+        commoditys.append({'number': commodity.number, 'title': commodity.title, 'description': commodity.description,'main_image':commodity.main_image,'price':min_price})
+
+    # 创建分页信息的响应
+    return jsonify({
+        'total': paginated_commoditys.total,
+        'pages': paginated_commoditys.pages,
+        'page': page,
+        'per_page': per_page,
+        'prev_page': paginated_commoditys.prev_num,
+        'next_page': paginated_commoditys.next_num,
+        'has_prev': paginated_commoditys.has_prev,
+        'has_next': paginated_commoditys.has_next,
+        'commoditys': commoditys
+    })
 
 
-
-
-
+# 快捷命令
+# ---------------------------------------------------------------------------------------
+# 初始化数据
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
 def initdb(drop):
-    """Initialize the database."""
+    """初始化数据库"""
     if drop:  # 判断是否输入了选项
         db.drop_all()
     db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
+    click.echo('已初始化数据库')  # 输出提示信息
 
-@app.errorhandler(404)  # 传入要处理的错误代码
-def page_not_found(e):  # 接受异常对象作为参数
-    user = User.query.first()
-    return render_template('404.html', user=user), 404  # 返回模板和状态码
-
-@app.route('/flash')
-def just_flash():
-    flash('的消息存储在session中?')
-    return redirect(url_for('index'))
+# 生成测试数据
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+def gendb():
+    """生成测试数据"""
+    faker = Faker(locale='zh_CN')
+    #货物
+    commodity = Commodity(
+        number = faker.bothify('#########'),
+        title = faker.text(max_nb_chars=10),
+        description = faker.text(max_nb_chars=30),
+        main_image = faker.image_url(),
+        images = faker.json(data_columns={'images':['image_url', 'image_url', 'image_url']}, num_rows=1)
+    )
+    db.session.add(commodity)
+    #型号和价格
+    model_price = ModelPrice(
+        type = faker.word(),
+        price = faker.bothify('##.##'),
+    )
+    db.session.add(model_price)
+    db.session.commit()
+    commodity.models.append(model_price)
+    db.session.commit()
+    # 物流车
+    expressage = Expressage(
+        title = faker.text(max_nb_chars=5),
+        description = faker.text(max_nb_chars=30),
+        name = faker.name(),
+        phone = faker.phone_number(),
+        license_number = faker.license_plate()
+    )
+    db.session.add(expressage)
+    db.session.commit()
+    user = User(
+        user_wechat = faker.vin(),
+        phone = faker.phone_number()
+    )
+    db.session.add(user)
+    consignee = Consignee(
+        address = faker.address(),
+        phone = faker.phone_number(),
+        name = faker.name(),
+        expressage_id = expressage.id
+    )
+    db.session.add(consignee)
+    order = Order(
+        number = faker.bothify('##################'),
+        commodity_number = commodity.number,
+        type = model_price.type,
+        conut = faker.bothify('#'),
+        address = consignee.address,
+        phone = consignee.phone,
+        expressage_id = consignee.expressage_id,
+        name = consignee.name
+    )
+    db.session.add(order)
+    db.session.commit()
+    user.orders.append(order)
+    user.consignees.append(consignee)
+    db.session.commit()
+    click.echo('已生成测试数据')  # 输出提示信息
